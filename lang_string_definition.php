5ec8753498fc84e2238fa0653f551a107c8fda70
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-01-27 18:03:01 -0600

    string definition

---
+++ lang_string_definition.php
@@ -0,0 +1,26 @@
+<?php
+
+    function diff_Strings(){
+
+        //select $_SESSION
+
+        switch($_SESSION['lang']){
+
+            case "en":
+                define(TXT, "Welcome to our site!");
+                define(LANG_CHOICE, "Choose a language");
+                break;
+            
+            case "fr":
+                define(TXT, "Bienvenue á notre site internet!");
+                define(LANG_CHOICE, "Choisis une langue");
+                break;
+
+            default:
+                define(TXT, "Welcome to our site!");
+                define(LANG_CHOICE, "Choose a language");
+                break;
+        }
+    }
+
+?>
