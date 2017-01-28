d31cfb7ce9e9810be20cff4510f831e840ae5ae8
Fabien Thiombiano <thiombianofabien@gmail.com>
2017-01-27 18:04:40 -0600

    string definition-modified

--- lang_string_definition.php
+++ lang_string_definition.php
@@ -8,17 +8,17 @@
 
             case "en":
                 define(TXT, "Welcome to our site!");
-                define(LANG_CHOICE, "Choose a language");
+                define(LANG_CHOICE, "Choose language");
                 break;
             
             case "fr":
                 define(TXT, "Bienvenue á notre site internet!");
-                define(LANG_CHOICE, "Choisis une langue");
+                define(LANG_CHOICE, "Langue choisie");
                 break;
 
             default:
                 define(TXT, "Welcome to our site!");
-                define(LANG_CHOICE, "Choose a language");
+                define(LANG_CHOICE, "Choose language");
                 break;
         }
     }
